e9640bc30705d5fd402a4e829b7f1ba85647fbcd
Danny Preussler <dpreussler@web.de>
2018-02-03 00:18:11 +0100

    code in again

--- lib/src/main/java/tv/sporttotal/toothpick/kotlin/ToothpickExtensions.kt
+++ lib/src/main/java/tv/sporttotal/toothpick/kotlin/ToothpickExtensions.kt
@@ -1,18 +1,18 @@
 package tv.sporttotal.toothpick.kotlin
 
+import toothpick.config.Binding
+import toothpick.config.Module
+import javax.inject.Provider
+import kotlin.reflect.KClass
 import kotlin.to as toPair
 
-//
-//inline fun <reified T> Module.bind(): Binding<T> = bind(T::class.java)
-//inline fun <reified T> Module.bindClass(target: () -> KClass<out Any>): Binding<T> = bind<T>().apply { to(target().java as Class<T>) }
-//inline fun <reified T> Module.bindClass(target: Class<out T>): Binding<T> = bind<T>().apply { to(target) }
-//inline fun <reified T> Module.bindClass(target: KClass<out Any>): Binding<T> = bind<T>().apply { to(target.java as Class<T>) }
-//inline fun <reified T> Module.bindInstance(target: () -> T): Binding<T> = bind<T>().apply { toInstance(target()) }
-//inline fun <reified T> Module.bindProvider(target: () -> Class<out Provider<T>>): Binding<T> = bind<T>().apply { toProvider(target()) }
-//inline fun <reified T> Module.bindProvider(target: KClass<out Provider<T>>): Binding<T> = bind<T>().apply { toProvider(target.java) }
-//inline fun <reified T> Module.bindProvider(target: Class<out Provider<T>>): Binding<T> = bind<T>().apply { toProvider(target) }
-//inline fun <reified T> Module.bindProviderInstance(target: () -> Provider<T>): Binding<T> = bind<T>().apply { toProviderInstance(target()) }
+inline fun <reified T> Module.bind(): Binding<T> = bind(T::class.java)
+inline fun <reified T> Module.bindClass(target: () -> KClass<out Any>): Binding<T> = bind<T>().apply { to(target().java as Class<T>) }
+inline fun <reified T> Module.bindClass(target: Class<out T>): Binding<T> = bind<T>().apply { to(target) }
+inline fun <reified T> Module.bindClass(target: KClass<out Any>): Binding<T> = bind<T>().apply { to(target.java as Class<T>) }
+inline fun <reified T> Module.bindInstance(target: () -> T): Binding<T> = bind<T>().apply { toInstance(target()) }
+inline fun <reified T> Module.bindProvider(target: () -> Class<out Provider<T>>): Binding<T> = bind<T>().apply { toProvider(target()) }
+inline fun <reified T> Module.bindProvider(target: KClass<out Provider<T>>): Binding<T> = bind<T>().apply { toProvider(target.java) }
+inline fun <reified T> Module.bindProvider(target: Class<out Provider<T>>): Binding<T> = bind<T>().apply { toProvider(target) }
+inline fun <reified T> Module.bindProviderInstance(target: () -> Provider<T>): Binding<T> = bind<T>().apply { toProviderInstance(target()) }
 
-class TEst {
-
-}
